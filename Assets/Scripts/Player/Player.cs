2ef946407ba96e0c96969f57dd6e02195f9c22ea
WarmSeam <158296722+WarmSeam@users.noreply.github.com>
2026-01-23 22:15:05 +0300

    work on adding strikes has begun

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -1,7 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
-[RequireComponent(typeof(PlayerInputHandler))]
+[RequireComponent(typeof(PlayerMovement), typeof(Rotator))]
 
 public class Player : MonoBehaviour
 {
+    [SerializeField] private InputReader _inputReader;
+
+    private PlayerMovement _movement;
+    private Rotator _rotator;
+
+    private void Awake()
+    {
+        _movement = GetComponent<PlayerMovement>();
+        _rotator = GetComponent<Rotator>();
+    }
+
+    private void OnEnable()
+    {
+        _inputReader.InputChanged += SetRotation;
+        _inputReader.InputChanged += _movement.Move;
+        _inputReader.RunPressed += _movement.Run;
+        _inputReader.JumpRequested += _movement.Jump;
+    }
+
+    private void OnDisable()
+    {
+        _inputReader.InputChanged -= SetRotation;
+        _inputReader.InputChanged -= _movement.Move;
+        _inputReader.RunPressed -= _movement.Run;
+        _inputReader.JumpRequested -= _movement.Jump;
+    }
+
+    private void SetRotation(float direction)
+    {
+        Vector3 targetDirection = new Vector3(direction, 0, 0) + transform.position;
+        _rotator.Rotate(targetDirection);
+    }
 }
